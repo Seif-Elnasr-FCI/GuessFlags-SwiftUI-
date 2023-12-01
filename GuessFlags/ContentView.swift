8ce379b8acca03a429791d98bf62ec31f36f78d5
seif amr mohammed <68325899+Seif-Elnasr-FCI@users.noreply.github.com>
2023-12-01 05:30:47 -0800

    Add files via upload

---
+++ GuessFlags/ContentView.swift
@@ -0,0 +1,100 @@
+//
+//  ContentView.swift
+//  GuessFlags
+//
+//  Created by seif elshahet on 30/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var countries = ["Estonia","France","Germany","Ireland","Poland","Spain","UK","Ukraine","US","Italy", "Nigeria","Monaco"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var score = 0
+        
+    var body: some View {
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ],center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the flag")
+                    .foregroundStyle(.white)
+                    .font(.largeTitle.bold())
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 10)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .font(.title.bold())
+                    .foregroundStyle(.white)
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(score)")
+        }
+        
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+            score -= 1
+            if score < 0 {
+                score = 0
+            }
+            
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessFlags/GuessFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagsApp.swift
+//  GuessFlags
+//
+//  Created by seif elshahet on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
